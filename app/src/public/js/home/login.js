2dd79702ea8df96b2407cdee6a6a2edbbf92df73
jihyun <looluu@naver.com>
2021-02-02 17:18:03 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+    
+loginBtn.addEventListener("click", login); //loginBtn 클릭되면 로그인 되게
+
+function login() { // id,pw에 들어있는 값을 가져와 줘야한다
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    // console.log(id.value); // id.value로 접근하면 입력한 해당값을 가져온다
+}

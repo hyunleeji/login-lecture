578a552b5fcff012355534bf605c1fc596e56cf9
jihyun <looluu@naver.com>
2021-02-03 01:03:22 +0900

    UserStorage 모델 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,26 @@
+"use strict";
+
+class UserStorage { // class 안에 선언할때는 const같은 선언자가 필요없다
+    //로그인 화면에서 id,pw값을 서버에서 인증하는 과정이 필요 해당데이터를 서버가 가지고 있어야한다.
+    static #users = { // class 자체에서 users에 접근하고자 할때는 static 을 선언하여 정적 변수로 만들어줘야한다.
+        id: ["worimIT", "나개발", "김팀장"],
+        psword: ["1234", "1324", "123456"],
+        name: ["우리밋", "나개발", "김팀장"],
+    };
+
+    static getUsers(...fields) { // class안에 method를 접근하려면 앞에 static을 넣어주어야한다
+        const users = this.#users; // 은닉화 된 private 변수인 #users 반환
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+}
+
+module.exports = UserStorage;
+
+// console.log(UserStorage.users); 이런식으로 내부데이터에 접근하는게 불가해야 하는게 맞다. 그래서
+// 다이렉트로 접근할수 없게 하기 위해 static users을 은닉화 #users = public한 변수에서 private 변수로 만들어 준다
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const UserStorage = require("../../models/UserStorage");
+
 const output = {
     home: (req, res) => { // const hello = function hello
         res.render("home/index");
@@ -10,34 +12,35 @@ const output = {
     },
 };
 
-//로그인 화면에서 id,pw값을 서버에서 인증하는 과정이 필요 해당데이터를 서버가 가지고 있어야한다.
-const users = {
-    id: ["worimIT", "나개발", "김팀장"],
-    psword: ["1234", "1324", "123456"],
-};
 
 //해당 데이터랑 프론트에서 전달한 데이터랑 인증하는 과정
 const process = {
     login: (req, res) => {
         const id = req.body.id,
         psword = req.body.psword;
+        const users = UserStorage.getUsers("id", "psword"); //console.log로 다이렉트로 가져온 UserStorage class 이용하기
 
+        const response = {};
         //아이디와 패스워드 검증
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             if (users.psword[idx] === psword) {// users psword idx가 프론트에서 전달한 psword가 같은지 여부를 판단하는 것.
-                return res.json({ // 로그인이 성공을 하게 되면 success: true라는 오브젝트를 json으로 만들어서 프론트엔드로 응답을 해주게 만드는 것.
-                    success: true,
-                });
+                response.success = true;
+                return res.json(response); // 로그인이 성공을 하게 되면 success: true라는 오브젝트를 json으로 만들어서 프론트엔드로 응답을 해주게 만드는 것.
             }
         }
 
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하셨습니다.",
-        });
-    },
-};
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다.";
+        return res.json(response);
+        },
+    };
+
+// 원래
+// return res.json({
+//     success: false,
+//     msg: "로그인에 실패하셨습니다.",
+// });
 
 //hello, login index.js에서 사용할수 있도록 모듈을 바깥으로 빼준다.
 module.exports = {

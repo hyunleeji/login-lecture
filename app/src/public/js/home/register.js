f6ca2513fe0383c9ac64743a1888a677a93e4273
jihyun <looluu@naver.com>
2021-02-03 19:01:13 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -2,20 +2,24 @@
 // html과 연결되어 있는 js파일 프론트화면에 보여질 화면이다
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
-    
-loginBtn.addEventListener("click", login); //loginBtn 클릭되면 로그인 되게
+    comfirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button")
 
-function login() { // id,pw에 들어있는 값을 가져와 줘야한다
+registerBtn.addEventListener("click", register); //registerBtn 클릭되면 로그인 되게
+
+function register() { // id,pw에 들어있는 값을 가져와 줘야한다
     const req = {
         id : id.value,
+        name : name.value,
         psword : psword.value,
+        comfirmPsword : comfirmPsword.value,
     };
-
     console.log(req); // 오브젝트 형태는 키와 값의 형태로 잘 주어져 있지만
-    console.log(JSON.stringify(req)); // json은 일반 문자열의 형태로 출력이 된다.
-    fetch("/login", { //body를 통해서 데이터를 전달해야하는데 http method 중에서 POST라는 메소드로 데이터를 전달해야한다.
+    // console.log(JSON.stringify(req)); // json은 일반 문자열의 형태로 출력이 된다.
+
+    fetch("/register", { //body를 통해서 데이터를 전달해야하는데 http method 중에서 POST라는 메소드로 데이터를 전달해야한다.
         method: "POST",
         headers: {
             "Content-Type": "application/json" //application한 형태로 json에 전달을 한다.
@@ -25,16 +29,16 @@ function login() { // id,pw에 들어있는 값을 가져와 줘야한다
         .then((res) => res.json())
         .then((res) => {
             if(res.success) {
-                location.href = "/";
+                location.href = "/login";
             } else {
                 alert(res.msg);
             }
         }) // 에러발생시 
         .catch((err) => {
-            console.error("로그인 중 에러 발생");
+            console.error("회원가입 중 에러 발생");
         });
     // console.log(id.value); // id.value로 접근하면 입력한 해당값을 가져온다
 }
 
-// fetch로 데이터가 서버로 전달이 되고, 이러한 데이터를 서버에서 받으려면 login이라는 경로
+// fetch로 데이터가 서버로 전달이 되고, 이러한 데이터를 서버에서 받으려면 register이라는 경로
 // POST라는 메소드로 데이터를 받을수 있는 API가 마련이 되어있어야 한다.

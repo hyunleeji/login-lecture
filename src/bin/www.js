09c1ec44e01c04fd4bcaac370c004079500c8e81
jihyun <looluu@naver.com>
2021-02-03 21:16:12 +0900

    루트의 src폴더 삭제

--- src/bin/www.js
+++
@@ -1,9 +0,0 @@
-//서버를 띄어주는 코드 복붙
-"use strict";
-
-const app = require("../app");
-const PORT = 3000;
-
-app.listen(PORT, () => {//원래 : app.listen(3000, () => { -> PORT라는 변수로 저장 // listen이라는 명령어로 서버를 띄울수 있다. 3000번 port로 열어달라. 2번째 
-    console.log("서버 가동");
-});
